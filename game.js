3ce705fc475824334855603eb5810e08195d88a8
Morganti86 <mmorganti86@gmail.com>
2024-08-02 14:05:54 +0200

    bomb added

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-
 let score = 0;
 let scorePanel = document.querySelector(".score");
 let commentsPanel = document.querySelector(".comments");
+const bomb = "url('./images/bomb.gif')";
 
 createEnemies();
 
@@ -22,10 +22,47 @@ function createEnemies() {
 }
 
 function handleEnemyClick(event) {
-  const points = parseInt(event.target.getAttribute("points"), 10);
+  const npc = event.target;
+  const points = parseInt(npc.getAttribute("points"), 10);
   score += points;
+
+  npc.style.backgroundImage = bomb;
+  npc.style.backgroundPosition = "center";
+
+  setTimeout(() => {
+    npc.style.visibility = "hidden";
+    checkPosition(npc);
+  }, 800);
+
   refreshScore(score);
-  event.target.style.backgroundColor = "green";
+}
+
+function checkPosition(npc) {
+  function update() {
+    let screen = document.querySelector(".screen");
+    let screenRect = screen.getBoundingClientRect();
+    let screenLeft = screenRect.left;
+
+    let npcRect = npc.getBoundingClientRect();
+    let npcRight = npcRect.right; // Bordo derecho del `NPC`
+
+    // Si el borde derecho del `NPC` está a la izquierda del borde derecho de la pantalla
+    if (npcRight > screenLeft) {
+      requestAnimationFrame(update);
+    } else {
+      console.log("sali de requestAnimationFrame");
+      console.log("npcRight: ", npcRight);
+      console.log("screenLeft: ", screenLeft);
+
+      npc.style.visibility = "visible"; // Volver a mostrar el NPC
+      let originalBackground = NPCS.find(
+        (n) => parseInt(npc.id) === n.id
+      ).backgroundImage;
+      npc.style.backgroundImage = originalBackground;
+    }
+  }
+
+  requestAnimationFrame(update);
 }
 
 function refreshScore(score) {
@@ -35,6 +72,11 @@ function refreshScore(score) {
 }
 
 function refreshComment(score) {
-  const comment = COMMENTS.find((comment) => score <= comment.score);
-  commentsPanel.textContent = comment.comment;
+  let comment = COMMENTS.slice()
+    .reverse()
+    .find((c) => score >= c.score);
+
+  if (comment) {
+    commentsPanel.textContent = comment.comment;
+  }
 }
