c7c9d770e74917738ff4a3b5be4b9eac3e8cdc01
Morganti86 <mmorganti86@gmail.com>
2024-08-07 13:18:49 +0200

    optimized images

--- LEVELS.js
+++ LEVELS.js
@@ -1,8 +1,10 @@
 const LEVELS = [
-  { level: 1, score: 0, background: "1.jpg" },
-  { level: 2, score: 2500, background: "2.jpg" },
-  { level: 3, score: 5000, background: "3.jpg" },
-  { level: 4, score: 10000, background: "4.jpg" },
-  { level: 5, score: 12000, background: "5.jpg" }, 
-  { level: 6, score: 15000, background: "6.jpg" },
+  { level: 1, score: 0, background: "1.webp" },
+  { level: 2, score: 2500, background: "2.webp" },
+  { level: 3, score: 5000, background: "3.webp" },
+  { level: 4, score: 10000, background: "4.webp" },
+  { level: 5, score: 12000, background: "5.webp" },
+  { level: 6, score: 15000, background: "6.webp" },
+  { level: 7, score: 18000, background: "7.webp" },
+  { level: 8, score: 20000, background: "8.webp" },
 ];
